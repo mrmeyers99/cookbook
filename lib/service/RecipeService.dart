13010f9fbc742cc056b015d4a18a2018272bb892
Michael Meyers <mrmeyers99@gmail.com>
2020-04-28 07:20:50 -0400

    Moved viewing/editing recipe logic into RecipeService

--- lib/service/RecipeService.dart
+++ lib/service/RecipeService.dart
@@ -1,19 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:home_cooked/model/recipe.dart';
 
 class RecipeService {
-  final CollectionReference recipes;
+  final CollectionReference _recipes;
+  final Firestore _db;
 
-  RecipeService(): this.recipes = Firestore.instance.collection("recipes");
+  RecipeService():
+        this._recipes = Firestore.instance.collection("recipes"),
+        this._db = Firestore.instance;
 
   Stream<QuerySnapshot> getRecipes(String uid, {
-    String sortBy = 'name', 
+    String sortBy = 'name',
     List filterBy = const [""],
-    String keywords = '', 
+    String keywords = '',
     int maxResults = 0
     }
     ) {
-    Query query = recipes.where("uid", isEqualTo: uid);
+    Query query = _recipes.where("uid", isEqualTo: uid);
     if (keywords != '' && keywords != null) {
       query = query.where("keywords", arrayContainsAny: keywords.toLowerCase().split(" "));
     }
@@ -26,4 +28,43 @@ class RecipeService {
     }
     return query.snapshots();
   }
+
+  Stream<DocumentSnapshot> getRecipe(String id) {
+    return _recipes.document(id).snapshots();
+  }
+
+  Future<Map<String, dynamic>> updateRecipe(String id, {name: String, ingredients: String, instructions}) {
+    var recipeRef = _recipes.document(id);
+    return _db.runTransaction((transaction) {
+      return transaction.get(recipeRef).then((recipeDoc) {
+        if (!recipeDoc.exists) {
+          throw "Recipe does not exist!";
+        }
+        transaction.update(recipeRef, {
+          "name": name,
+          "ingredients": ingredients,
+          "instructions": instructions,
+          "keywords": _buildKeywords(name, ingredients),
+          "updated_at": FieldValue.serverTimestamp()
+        });
+      });
+    });
+  }
+
+  List<String> _buildKeywords(String name, List<String> ingredients) {
+    var wordList = name.split(" ");
+    //todo since we add ingredient parsing, we should probably ignore quantities and units
+    //todo ignore punctuation in words?  smores vs s'mores?
+    ingredients.where((s) => !s.startsWith("//")).forEach((s) => wordList.addAll(s.split(" ")));
+    var wordSet = wordList.map((s) => s.toLowerCase()).toSet();
+
+    var subWordSet = Set<String>();
+    wordSet.forEach((word) {
+      for(var i = 2; i <= word.length; i++) {
+        subWordSet.add(word.substring(0, i));
+      }
+    });
+    return subWordSet.toList();
+  }
+
 }
--- lib/ui/screens/edit_recipe.dart
+++ lib/ui/screens/edit_recipe.dart
@@ -1,10 +1,11 @@
 
 import 'dart:core';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:home_cooked/locator.dart';
 import 'package:home_cooked/model/recipe.dart';
+import 'package:home_cooked/service/RecipeService.dart';
 
 class EditRecipeScreen extends StatefulWidget {
 
@@ -25,31 +26,17 @@ class _EditRecipeScreenState extends State<EditRecipeScreen> {
   final TextEditingController nameController;
   final TextEditingController ingredientsController;
   final TextEditingController instructionsController;
+  final RecipeService _recipeService;
 
   _EditRecipeScreenState(this.recipe):
         this.nameController = TextEditingController(text: recipe.name),
         this.ingredientsController = TextEditingController(text: recipe.ingredients.join("\n")),
-        this.instructionsController = TextEditingController(text: recipe.instructions.join("\n"));
+        this.instructionsController = TextEditingController(text: recipe.instructions.join("\n")),
+        this._recipeService = locator.get<RecipeService>();
 
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
   final _formKey = GlobalKey<FormState>();
-
-  List<String> _buildKeywords(String name, List<String> ingredients) {
-    var wordList = name.split(" ");
-    //todo since we add ingredient parsing, we should probably ignore quantities and units
-    //todo ignore punctuation in words?  smores vs s'mores?
-    ingredients.where((s) => !s.startsWith("//")).forEach((s) => wordList.addAll(s.split(" ")));
-    var wordSet = wordList.map((s) => s.toLowerCase()).toSet();
-
-    var subWordSet = Set<String>();
-    wordSet.forEach((word) {
-      for(var i = 2; i <= word.length; i++) {
-        subWordSet.add(word.substring(0, i));
-      }
-    });
-    return subWordSet.toList();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -102,22 +89,10 @@ class _EditRecipeScreenState extends State<EditRecipeScreen> {
                           // Validate returns true if the form is valid, or false
                           // otherwise.
                           if (_formKey.currentState.validate()) {
-                            var db = Firestore.instance;
-                            var recipeRef = db.collection("recipes").document(recipe.id);
-                            db.runTransaction((transaction) {
-                              return transaction.get(recipeRef).then((recipeDoc) {
-                                if (!recipeDoc.exists) {
-                                  throw "Recipe does not exist!";
-                                }
-                                transaction.update(recipeRef, {
-                                  "name": nameController.text,
-                                  "ingredients": ingredientsController.text.split("\n"),
-                                  "instructions": instructionsController.text.split("\n"),
-                                  "keywords": _buildKeywords(nameController.text, ingredientsController.text.split("\n")),
-                                  "updated_at": FieldValue.serverTimestamp()
-                                });
-                              });
-                            })
+                            _recipeService.updateRecipe(recipe.id,
+                                name: nameController.text,
+                                ingredients: ingredientsController.text.split("\n"),
+                                instructions: instructionsController.text.split("\n"))
                             .then((result) {
                               Navigator.pop(context);
                             });
