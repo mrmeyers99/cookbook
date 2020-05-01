1d7a569b558af55164366586e3f7311c35d406b2
Michael Meyers <mrmeyers99@gmail.com>
2020-05-01 07:08:30 -0400

    don't pass uid around anymore since _TagScreenState can get it from TagScreen

--- lib/ui/screens/tags.dart
+++ lib/ui/screens/tags.dart
@@ -1,4 +1,3 @@
-
 import 'dart:core';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -6,56 +5,46 @@ import 'package:home_cooked/service/RecipeService.dart';
 import 'package:home_cooked/service/UserService.dart';
 import '../../locator.dart';
 
-
-
 class TagScreen extends StatefulWidget {
-
   final uid;
 
   TagScreen(this.uid);
 
-
   @override
   State<StatefulWidget> createState() {
-    return _TagScreenState(uid);
+    return _TagScreenState();
   }
-
 }
 
-
-
 class _TagScreenState extends State<TagScreen> {
-
   final UserService userService;
   final RecipeService recipeService;
-  final uid;
   var selectedIdx = [];
   List<String> allTags;
 
-  _TagScreenState(this.uid):
-    this.userService = locator.get<UserService>(),
-    this.recipeService = locator.get<RecipeService>();
-
-
+  _TagScreenState()
+      : this.userService = locator.get<UserService>(),
+        this.recipeService = locator.get<RecipeService>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Choose Tags'),
-        actions: <Widget>[
-          IconButton(
-                  icon: Icon(Icons.check),
-                  onPressed: () {
-                    if (selectedIdx.length >0) {
-                    Navigator.pop(context,selectedIdx.map((index) => allTags[index]).toList());
-                    } else {Navigator
-                    .pop(context,[""]); // If none selected, don't filter.
-                    }
-                  },
-                ), 
-        ],
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Choose Tags'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: () {
+                if (selectedIdx.length > 0) {
+                  Navigator.pop(context,
+                      selectedIdx.map((index) => allTags[index]).toList());
+                } else {
+                  Navigator.pop(context, [""]); // If none selected, don't filter.
+                }
+              },
+            ),
+          ],
         ),
         body: Container(
           width: double.infinity,
@@ -66,27 +55,27 @@ class _TagScreenState extends State<TagScreen> {
               if (snapshot.hasData) {
                 allTags = snapshot.data;
                 return Center(
-                  child: ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) => FilterChip(
-                      label:Text(snapshot.data[index]),
-                      onSelected: (bool value) {
+                    child: ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) => FilterChip(
+                    label: Text(snapshot.data[index]),
+                    onSelected: (bool value) {
+                      setState(() {
                         if (selectedIdx.contains(index)) {
                           selectedIdx.remove(index);
                         } else {
                           selectedIdx.add(index);
                         }
-                      setState(() {});
-                      },
+                      });
+                    },
                     selected: selectedIdx.contains(index),
                     selectedColor: Colors.deepOrange,
                     labelStyle: TextStyle(
-                     color: Colors.white,
-                     ),
-                    backgroundColor: Colors.green,
+                      color: Colors.white,
                     ),
-                  )
-                );
+                    backgroundColor: Colors.green,
+                  ),
+                ));
               } else {
                 return Center(
                   child: CircularProgressIndicator(),
@@ -94,11 +83,8 @@ class _TagScreenState extends State<TagScreen> {
               }
             },
           ),
-        )
-    );
-    }
+        ));
+  }
 
-    Future<List<String>> getFutureTags() async =>
-      await recipeService.getAllTags(uid);
-
+  Future<List<String>> getFutureTags() => recipeService.getAllTags(widget.uid);
 }
